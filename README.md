ec5801a66201b04f2292d1d8046db6215f3f2b7b
hrntsm <contact@hrntsm.com>
2021-05-06 22:03:19 +0900

    Add 3dm layer

--- README.md
+++ README.md
@@ -2,9 +2,14 @@
 
 This is a model viewer that uses threejs. Check it out [here](https://hiron.dev/threejs-modelviewer/)!
 
-IFC part is based on [agviegas/ifcjs-hello-world](https://github.com/agviegas/ifcjs-hello-world). Thanks Antonio González Viegas!!!
-
 ## Support
 
 - IFC
 - 3dm
+
+## Credit
+
+IFC part is based on [agviegas/ifcjs-hello-world](https://github.com/agviegas/ifcjs-hello-world).  
+Thanks Antonio González Viegas!!!
+
+3dm part is based on [three.js/examples/webgl_loader_3dm.html](https://github.com/mjurczyk/three.js/blob/sandbox/examples/webgl_loader_3dm.html)
--- src/app.js
+++ src/app.js
@@ -1,50 +1,43 @@
-import {
-    AmbientLight,
-    AxesHelper,
-    Color,
-    DirectionalLight,
-    GridHelper,
-    PerspectiveCamera,
-    Scene,
-    WebGLRenderer,
-} from "three";
+import * as THREE from "three/build/three.module";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { IFCLoader } from "three/examples/jsm/loaders/IFCLoader";
 import { Rhino3dmLoader } from "three/examples/jsm/loaders/3DMLoader";
 
-const scene = new Scene();
-scene.background = new Color(0xaaaaaa);
+import { GUI } from "three/examples/jsm/libs/dat.gui.module";
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xaaaaaa);
 
 const size = {
     width: window.innerWidth,
     height: window.innerHeight,
 };
 
-const camera = new PerspectiveCamera(75, size.width / size.height);
+const camera = new THREE.PerspectiveCamera(75, size.width / size.height);
 camera.position.z = 3;
 camera.position.y = 3;
 camera.position.x = 3;
 
 const lightColor = 0xffffff;
 
-const ambientLight = new AmbientLight(lightColor, 0.5);
+const ambientLight = new THREE.AmbientLight(lightColor, 0.5);
 scene.add(ambientLight);
 
-const directionalLight = new DirectionalLight(lightColor, 1);
+const directionalLight = new THREE.DirectionalLight(lightColor, 1);
 directionalLight.position.set(0, 10, 0);
 directionalLight.target.position.set(-5, 0, 0);
 scene.add(directionalLight);
 scene.add(directionalLight.target);
 
 const threeCanvas = document.getElementById("three-canvas");
-const renderer = new WebGLRenderer({ canvas: threeCanvas });
+const renderer = new THREE.WebGLRenderer({ canvas: threeCanvas });
 renderer.setSize(size.width, size.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-const grid = new GridHelper(50, 30);
+const grid = new THREE.GridHelper(50, 30);
 scene.add(grid);
 
-const axes = new AxesHelper();
+const axes = new THREE.AxesHelper();
 axes.material.depthTest = false;
 axes.renderOrder = 1;
 scene.add(axes);
@@ -86,8 +79,11 @@ input.addEventListener(
             ifcLoader.load(modelURL, (geometry) => scene.add(geometry));
         }
         else if (ext === "3dm") {
-            alert("NOTICE: This viewer is only support Mesh object in 3dm, not support like NURBS objects.");
-            rhino3dmLoader.load(modelURL, (geometry) => scene.add(geometry), false, false);
+            alert("NOTICE: This viewer is only supported Mesh object in 3dm, not support like NURBS objects.");
+            rhino3dmLoader.load(modelURL, function (objct) {
+                scene.add(objct);
+                initGUI(objct.userData.layers);
+            });
         }
     },
     false
@@ -97,4 +93,29 @@ function getExt(filename) {
     var pos = filename.lastIndexOf('.');
     if (pos === -1) return '';
     return filename.slice(pos + 1);
+}
+
+function initGUI(layers) {
+    const gui = new GUI({ width: 300 });
+    const layersControl = gui.addFolder('layers');
+    layersControl.open();
+
+    for (let i = 0; i < layers.length; i++) {
+        const layer = layers[i];
+        layersControl.add(layer, 'visible').name(layer.name).onChange(function (val) {
+            const name = this.object.name;
+
+            scene.traverse(function (child) {
+                if (child.userData.hasOwnProperty('attributes')) {
+                    if ('layerIndex' in child.userData.attributes) {
+                        const layerName = layers[child.userData.attributes.layerIndex].name;
+                        if (layerName === name) {
+                            child.visible = val;
+                            layer.visible = val;
+                        }
+                    }
+                }
+            });
+        });
+    }
 }
