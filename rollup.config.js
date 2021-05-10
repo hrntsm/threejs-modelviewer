532a868147385d29107e52853d253cd52ece1c66
hrntsm <contact@hrntsm.com>
2021-05-10 22:07:51 +0900

    Add rollup-terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,19 @@
 import nodeResolve from "rollup-plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
+
+const plugins = [
+  nodeResolve(),
+];
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push(terser());
+}
 
 export default {
   input: "src/app.js",
   output: {
     file: "bundle.js",
-    format: "cjs",
+    format: "cjs"
   },
-  plugins: [nodeResolve()],
+  plugins
 };
